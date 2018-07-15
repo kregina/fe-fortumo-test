566f98961b7b253fe4f16dde5a9ac5a324ac669b
Kelly Silva <kelly@kellyregina.com.br>
2018-07-15 20:15:15 -0300

    implement something awesome tests

--- src/app/core/something-awesome/something-awesome.service.spec.ts
+++ src/app/core/something-awesome/something-awesome.service.spec.ts
@@ -11,13 +11,17 @@ import { RouterTestingModule } from "@angular/router/testing";
 describe("SomethingAwesomeService", () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  let service: SomethingAwesomeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
         RouterTestingModule.withRoutes([
-          { path: 'landing-page', loadChildren: './landing-page/landing-page.module#LandingPageModule' }
+          {
+            path: "landing-page",
+            loadChildren: "./landing-page/landing-page.module#LandingPageModule"
+          }
         ])
       ],
       providers: [SomethingAwesomeService]
@@ -25,12 +29,44 @@ describe("SomethingAwesomeService", () => {
 
     httpClient = TestBed.get(HttpClient);
     httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(SomethingAwesomeService);
   });
 
-  it("should be created", inject(
-    [SomethingAwesomeService],
-    (service: SomethingAwesomeService) => {
-      expect(service).toBeTruthy();
-    }
-  ));
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should return do Something Awesome", () => {
+    const firstResponse = {
+      activity: "xhr",
+      activity_url: "http://localhost:3000/last_endpoint"
+    };
+
+    const lastResponse = {
+      activity: "redirect",
+      activity_url: "/landing-page"
+    };
+
+    const responses = [];
+    service.doSomethingAwesome().subscribe((response) => {
+      responses.push(response);
+    });
+
+    const req1 = httpTestingController.expectOne(
+      "http://localhost:3000/first_endpoint"
+    );
+
+    req1.flush(firstResponse);
+
+    const req2 = httpTestingController.expectOne(
+      "http://localhost:3000/last_endpoint"
+    );
+
+    req2.flush(lastResponse);
+
+    expect(responses[0]).toBe(firstResponse);
+    expect(responses[1]).toBe(lastResponse);
+
+    httpTestingController.verify();
+  });
 });
